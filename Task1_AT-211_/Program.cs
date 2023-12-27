0535542dfe055216ce9fed0c05ca8687a2e7c473
Gaoraychuk K <91251268+KostyaG5@users.noreply.github.com>
2023-12-27 20:58:31 +0200

    Added variables for real numbers

--- Task1_AT-211_/Program.cs
+++ Task1_AT-211_/Program.cs
@@ -1,9 +1,15 @@
 ﻿namespace Task1_AT_211_
+///Вариант - 5. Реализовать функцию вычисления суммы двух вещественных чисел .
+///Исходные данные вводятся при помощи генератора случайных чисел(с возможностью получения как положительных, так и отрицательных значений)
+
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+
+            Random random = new ();
+            double number1 = random.NextDouble()*50-25 , number2 = random.NextDouble() * 50 - 25;
             Console.WriteLine("Hello, World!");
         }
     }

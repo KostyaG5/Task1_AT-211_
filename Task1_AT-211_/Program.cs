ca9e0b1987de96ca51839fdeca9adab3ae4d0722
Gaoraychuk K <91251268+KostyaG5@users.noreply.github.com>
2023-12-27 21:18:03 +0200

    Final Version

--- Task1_AT-211_/Program.cs
+++ Task1_AT-211_/Program.cs
@@ -5,17 +5,25 @@
 {
     internal class Program
     {
+        /// <summary>
+        /// Вычисления суммы двух вещественных чисел
+        /// </summary>
+        /// <param name="number1">Первое вещественное число</param>
+        /// <param name="number2">Второе вещественное число</param>
+        /// <returns>Сумму двух вещественных чисел</returns>
+        
         private static double SumofTwoRealNumbers(double number1, double number2)
         {
                return number1 + number2;
         }
 
-
+       
         static void Main(string[] args)
         {
 
             Random random = new ();
             double number1 = random.NextDouble()*50-25 , number2 = random.NextDouble() * 50 - 25;
+
             Console.WriteLine($"{number1} + {number2} = {SumofTwoRealNumbers(number1, number2)}") ;
         }
     }

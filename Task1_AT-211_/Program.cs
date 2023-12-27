b850f635af09cf432e79c0aa4dff3f633546d537
Gaoraychuk K <91251268+KostyaG5@users.noreply.github.com>
2023-12-27 20:45:39 +0200

    Добавьте файлы проекта.

---
+++ Task1_AT-211_/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task1_AT_211_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
